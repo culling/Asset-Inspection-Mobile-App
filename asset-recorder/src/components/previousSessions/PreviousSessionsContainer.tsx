a5df91a7a556d844068250a38b47da44021bfe49
culling <culling66@gmail.com>
2020-08-26 10:57:43 +1200

    Add IonSpinner
    Add IonSpinner

--- asset-recorder/src/components/previousSessions/PreviousSessionsContainer.tsx
+++ asset-recorder/src/components/previousSessions/PreviousSessionsContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../Default.css';
-import { IonList, IonContent, IonToast } from '@ionic/react';
+import { IonList, IonContent, IonToast, IonSpinner } from '@ionic/react';
 import AssetListItem from '../assetListItem/AssetListItem';
 
 import { Cloud } from '../../dataLocations';
@@ -11,11 +11,13 @@ interface ContainerProps {
 
 const PreviousSessionsContainer: React.FC<ContainerProps> = ({ settings }) => {
   const [previousAssets, setPreviousAssets] = useState([]);
+  const [successfulDownload, setSuccessfulDownload] = useState(false);
   const [successfulDownloadToast, setSuccessfulDownloadToast] = useState(false);
   const [noRemoteAssetsToast, setNoRemoteAssetsToast] = useState(false);
   const [loadingAssetsToast, setLoadingAssetsToast] = useState(true);
 
   let isRendered = useRef(false);
+
 
   useEffect(() => {
     console.log("settings: ", settings);
@@ -24,6 +26,7 @@ const PreviousSessionsContainer: React.FC<ContainerProps> = ({ settings }) => {
         setNoRemoteAssetsToast(true);
       } else {
         setPreviousAssets(previousAssets);
+        setSuccessfulDownload(true);
         setSuccessfulDownloadToast(true);
       }
     });
@@ -34,11 +37,17 @@ const PreviousSessionsContainer: React.FC<ContainerProps> = ({ settings }) => {
 
   return (
     <IonContent>
-      <IonList inset={true}>
-        {Array.isArray(previousAssets) && previousAssets.map((previousAsset, i) =>
-          <AssetListItem key={i} asset={previousAsset} />
-        )}
-      </IonList>
+      {
+        successfulDownload ?
+          <IonList inset={true}>
+            {Array.isArray(previousAssets) && previousAssets.map((previousAsset, i) =>
+              <AssetListItem key={i} asset={previousAsset} />
+            )}
+          </IonList> :
+          <div style={{ "height": "100%", marginLeft: "auto", marginRight: "auto", width:"100%", verticalAlign:"middle", alignItems:"center", justifyContent:"center", display: "flex" }}>
+            <IonSpinner name="crescent" style={{ "height": "100%", width:"10%",  verticalAlign:"middle", alignItems:"center", justifyContent:"center"  }} />
+          </div>
+      }
 
       <IonToast
         isOpen={loadingAssetsToast}
