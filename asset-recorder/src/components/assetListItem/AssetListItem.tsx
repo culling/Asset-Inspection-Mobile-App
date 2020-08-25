63ea36a517ad6a1f45eee5305b3c15077cd8fef7
culling <culling66@gmail.com>
2020-08-25 15:04:48 +1200

    Add expand on click for Asset List Item

--- asset-recorder/src/components/assetListItem/AssetListItem.tsx
+++ asset-recorder/src/components/assetListItem/AssetListItem.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Default.css';
 import { IonLabel, IonItem } from '@ionic/react';
 
 
 interface ContainerProps {
-    children?:React.ReactNode;
+    children?: React.ReactNode;
     asset: any;
 }
 
-const AssetListItem: React.FC<ContainerProps> = (
-    props: { children?: React.ReactNode,
-    asset: any }) => {
+const AssetListItem: React.FC<ContainerProps> = (props: { children?: React.ReactNode, asset: any }) => {
+    const [expandItem, setExpandItem] = useState(false);
+    const date = new Date(parseInt(props.asset.inspection_time));
+    console.log(date.toISOString());
     return (
-        <IonItem>
-            {props.children}
-            <IonLabel>{props.asset.assetIdText}</IonLabel>
-            <p>{props.asset.serialNumberText}</p>
-        </IonItem>
+        <div>
+            {expandItem ?
+                <IonLabel>
+                    <>{props.children}{props.asset.assetIdText}</>
+                    <br />
+                    <p>Details</p>
+                    <p>Asset Id: {props.asset.assetIdText}</p>
+                    <p>Serial Number: {props.asset.serialNumberText}</p>
+                    <p>Latitude: {props.asset.latitude}</p>
+                    <p>Longitude: {props.asset.longitude}</p>
+                    <p>Inspection Time: {date.toISOString()}</p>
+                <br />
+                </IonLabel> :
+                <IonItem onClick={() => { setExpandItem(!expandItem) }}>
+                    <IonLabel>
+                        {props.asset.assetIdText}
+                        <p>{props.asset.serialNumberText}</p>
+                    </IonLabel>
+                </IonItem>
+            }
+
+
+
+
+        </div>
     )
 }
 
